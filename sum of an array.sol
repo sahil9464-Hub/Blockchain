db713c33662a9740f17050c11cf2ff3123910bfa
sahil9464-Hub <sharmasahil1806@gmail.com>
2026-09-11 12:09:26 +0530

    Update sum of an array.sol

--- sum of an array.sol
+++ sum of an array.sol
@@ -1,28 +1,15 @@
-// SPDX-License-Identifier: MIT
+ // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract IntegerArray{
+contract IntegerArray {
 
-    uint[] private numbers;
-
-    // Set the array
-    function setNumbers(uint[] memory _numbers) public{
-        numbers = _numbers;
-    }
-
-    // Get the entire array
-    function getNumbers() public view returns (uint[] memory){
-        return numbers;
-    }
-
-    // Calculate sum of the array
-    function getSum() public view returns (uint){
+    function sumArray(uint[] memory numbers) public pure returns (uint) {
         uint sum = 0;
 
-        for (uint i = 0; i < numbers.length; i++){
+        for (uint i = 0; i < numbers.length; i++) {
             sum += numbers[i];
         }
 
         return sum;
     }
-}
+}++
